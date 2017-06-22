fca762ca1c19a6c4c0e0aaa736f445846371b8a4
李铭昕 <715557344@qq.com>
2017-06-22 20:47:26 +0800

    * 增加after方法

--- tests/Str/BaseTest.php
+++ tests/Str/BaseTest.php
@@ -150,5 +150,14 @@ class BaseTest extends TestCase
 
     }
 
+    public function testStrAfter()
+    {
+        $this->assertEquals('nah', Str::after('hannah', 'han'));
+        $this->assertEquals('nah', Str::after('hannah', 'n'));
+        $this->assertEquals('nah', Str::after('ééé hannah', 'han'));
+        $this->assertEquals('hannah', Str::after('hannah', 'xxxx'));
+        $this->assertEquals('hannah', Str::after('hannah', ''));
+    }
+
 
 }
